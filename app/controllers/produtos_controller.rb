3b58b048a8c9f2ac002bd573e60a293e42884624
Aram Sperduti Menocci <aramsm3@gmail.com>
2017-03-20 22:36:46 -0300

    Adicionada ações privadas, otimizado o controller da classe Produtos e adicionado partial _form

--- app/controllers/produtos_controller.rb
+++ app/controllers/produtos_controller.rb
@@ -1,5 +1,7 @@
 class ProdutosController < ApplicationController
 
+	before_action :set_produto, only: [:edit, :update, :destroy]
+
 	def index
 		@produtos_por_nome = Produto.order(:nome).limit 5
 		@produtos_por_preco = Produto.order(:preco).limit 2
@@ -10,24 +12,51 @@ class ProdutosController < ApplicationController
 		@departamentos = Departamento.all
 	end
 
+	def edit
+		render_(:edit)
+	end
+
+	def update
+		if @produto.update produto_params
+			flash[:notice] = "Produto atualizado com sucesso!" 
+        	redirect_to root_url
+    	else
+    		render_(:edit)
+    	end
+	end
+
     def create
-    	@produto = Produto.new params.require(:produto).permit :nome, :preco, :descricao, :quantidade, :departamento_id
+    	@produto = Produto.new produto_params
         if @produto.save
         	flash[:notice] = "Produto criado com sucesso!" 
         	redirect_to root_url
     	else
-    		@departamentos = Departamento.all
-    		render :new
+    		render_(:new)
     	end
     end
 
     def destroy
-    	Produto.destroy params[:id]
+    	@produto.destroy
     	redirect_to root_url
     end
 
     def busca
     	@produtos = Produto.where "nome like ?", "%#{params[:nome]}"
     end
+
+
+    private
+	    def set_produto
+	    	@produto = Produto.find params[:id]
+	    end
+
+	    def render_(view)
+			@departamentos = Departamento.all
+	    	render view
+		end
+
+		def produto_params
+	        params.require(:produto).permit :nome, :preco, :descricao, :quantidade, :departamento_id
+	    end
 
 end
